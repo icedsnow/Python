17c1bb45524cd130a096cc7e213791bf04a233bb
Adam Logan <mrclassifiedo@yahoo.com>
2017-05-20 15:18:40 -0600

    updated to translate url in var

--- pychal1.py
+++ pychal1.py
@@ -1,8 +1,10 @@
 import string
 
 text = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
-
+url = "map"
 intab = 'abcdefghijklmnopqrstuvwxyz'
 outab = 'cdefghijklmnopqrstuvwxyzab'
 
 print(text.translate(str.maketrans(intab, outab)))
+
+print(url.translate(str.maketrans(intab, outab)))
